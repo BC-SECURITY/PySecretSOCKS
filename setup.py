923adf9d1b00cf398aee08ab72262cdd3d71ab14
Vince Rose <vrose04@gmail.com>
2023-10-20 21:58:27 -0700

    typo

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ setup(
     classifiers=[
         'License :: OSI Approved :: MIT License',
         'Topic :: Security'
-    ]
+    ],
     # TODO: This library should be updated to migrate from pyasyncore to asyncio
     # asyncore was deprecated in python 3.6 and removed in python 3.12. This import
     # is a patch to allow this library to be used in python 3.12 and above.
